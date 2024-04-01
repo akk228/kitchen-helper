e4339dbfbeb450c56036064e5587193d64821662
akk228 <104084354+akk228@users.noreply.github.com>
2024-04-01 22:21:58 +0200

    Adds query to get ready to go reciepes

---
+++ dotNetCoreApi/FridgeAndRecipesAPI/CookingHelper/CookingQuery.cs
@@ -0,0 +1,23 @@
+﻿using FridgeAndRecipesStorage.KitchenHelper;
+using FridgeAndRecipesStorage.Recipies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FridgeAndRecipesAPI.CookingHelper;
+[ApiController]
+[Route("Cooking")]
+public class CookingQuery : ControllerBase
+{
+    private readonly KitchenHelper _kitchenHelper;
+
+    public CookingQuery(KitchenHelper kitchenHelper)
+    {
+        _kitchenHelper = kitchenHelper;
+    }
+
+    [HttpGet]
+    [Route("ReadyToGo")]
+    public IEnumerable<Recipe> GetReadyToGoRecipes()
+    {
+        return _kitchenHelper.FindReadyToGoRecipes();
+    }
+}
