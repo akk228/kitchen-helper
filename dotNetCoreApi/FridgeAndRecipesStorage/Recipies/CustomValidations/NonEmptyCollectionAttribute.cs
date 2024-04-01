6656a4e572fa2c210fd2c1cfe65a89cca6411c4e
akk228 <104084354+akk228@users.noreply.github.com>
2024-04-01 14:31:37 +0200

    Adds custom attribute checking for non emptiness of collection

---
+++ dotNetCoreApi/FridgeAndRecipesStorage/Recipies/CustomValidations/NonEmptyCollectionAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using BindingFlags = System.Reflection.BindingFlags;
+
+namespace FridgeAndRecipesStorage.Recipies.CustomValidations;
+
+public class NonEmptyCollectionAttribute : RequiredAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (!base.IsValid(value))
+        {
+            return false;
+        }
+        
+        try
+        {
+            var enumerable = value as IEnumerable;
+            
+            foreach (var obj in enumerable)
+            {
+                return true;
+            }
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+
+        return false;
+    }
+}
