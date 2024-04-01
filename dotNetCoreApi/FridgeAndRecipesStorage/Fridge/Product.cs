a8e2413775a9894c5fbdca226b7d0d0f6cfe1ce7
akk228 <104084354+akk228@users.noreply.github.com>
2024-04-01 14:30:55 +0200

    Adds validations to DTOs

--- dotNetCoreApi/FridgeAndRecipesStorage/Fridge/Product.cs
+++ dotNetCoreApi/FridgeAndRecipesStorage/Fridge/Product.cs
@@ -5,30 +5,20 @@ namespace FridgeAndRecipesStorage.Fridge;
 
 public enum Units
 {
-    gr,
-    ml,
-    unit
+    gr = 0,
+    ml = 1,
+    unit = 2
 }
 
 public class Product
 {
+    [Required]
     public string Name { get; set; }
+    
+    [Range(1, int.MaxValue, ErrorMessage = "Amount must be greater than 0")]
     public int Amount { get; set; }
+    
+    [Required, EnumDataType(typeof(Units))]
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public Units MeasurmentUnit { get; set; }
-
-    public bool UseSomeAmountOfProduct(int amount)
-    {
-        var newAmount = Amount - amount;
-        if (newAmount < 0)
-        {
-            return false;
-        } else if (newAmount == 0)
-        {
-            
-        }
-        
-        Amount -= amount;
-        return true;
-    }
 }
--- dotNetCoreApi/FridgeAndRecipesStorage/Recipies/Recipe.cs
+++ dotNetCoreApi/FridgeAndRecipesStorage/Recipies/Recipe.cs
@@ -1,16 +1,17 @@
 ﻿using FridgeAndRecipesStorage.Fridge;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using FridgeAndRecipesStorage.Recipies.CustomValidations;
 
 namespace FridgeAndRecipesStorage.Recipies
 {
     public class Recipe
     {
+        [Required]
         public string Name { get; set; }
+        
         public string? Description { get; set; }
+        
+        [NonEmptyCollection(ErrorMessage = "You can't create recipes out of thin air!")]
         public IList<Product> Ingredients { get; set; }
     }
 }
