e8c1edd99b15e95feeda7823310c51d50c4a3baa
akk228 <104084354+akk228@users.noreply.github.com>
2024-04-01 22:21:38 +0200

    adds class to look up recipes that can be cooked from fridge's content

---
+++ dotNetCoreApi/FridgeAndRecipesStorage/KitchenHelper/KitchenHelper.cs
@@ -0,0 +1,48 @@
+﻿using FridgeAndRecipesStorage.Recipies;
+
+namespace FridgeAndRecipesStorage.KitchenHelper;
+
+public class KitchenHelper
+{
+    private readonly Fridge.Fridge _fridge;
+    private readonly RecipesCollection _recipesCollection;
+
+    public KitchenHelper(Fridge.Fridge fridge, RecipesCollection recipesCollection)
+    {
+        _fridge = fridge;
+        _recipesCollection = recipesCollection;
+    }
+
+    public IEnumerable<Recipe> FindReadyToGoRecipes()
+    {
+        var products = _fridge
+            .GetAllProducts()
+            .ToDictionary(
+                product => product.Name,
+                product => product,
+                StringComparer.OrdinalIgnoreCase);
+
+        var readyToGoRecipes = _recipesCollection
+            .GetRecipes()
+            .ToDictionary(
+                recipe => recipe.Name,
+                recipe => recipe,
+                StringComparer.OrdinalIgnoreCase);
+
+        foreach (var recipe in readyToGoRecipes)
+        {
+            foreach (var ingredient in recipe.Value.Ingredients)
+            {
+                if (!products.ContainsKey(ingredient.Name))
+                {
+                    readyToGoRecipes.Remove(recipe.Value.Name);
+                }else if (ingredient.Amount > products[ingredient.Name].Amount)
+                {
+                    readyToGoRecipes.Remove(recipe.Value.Name);
+                }
+            }
+        }
+
+        return readyToGoRecipes.Select(recipe => recipe.Value);
+    }
+}
